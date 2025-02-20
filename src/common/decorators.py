576fb97486de7205ce4c498b158e4a658d2eca96
kauelima21 <kaueslim@outlook.com>
2025-02-20 18:03:25 -0300

    feat: add errors and make `response_json` able to handle errors

--- src/common/decorators.py
+++ src/common/decorators.py
@@ -1,16 +1,27 @@
 import json
 from functools import wraps
 
+from common.errors import BaseError
+
 
 def response_json(handler):
     @wraps(handler)
     def transform_response(*args, **kwargs):
-        response = handler(*args, **kwargs)
-        return {
-            "statusCode": response["status_code"],
-            "body": json.dumps(response.get("body")),
+        response_dict = {
             "isBase64Encoded": False,
             "headers": {"Content-Type": "application/json"},
+        }
+
+        try:
+            response = handler(*args, **kwargs)
+        except BaseError as error:
+            response = {"status_code": error.status_code,
+                        "body": error.to_dict()}
+
+        return {
+            **response_dict,
+            "statusCode": response["status_code"],
+            "body": json.dumps(response.get("body")),
         }
 
     return transform_response
